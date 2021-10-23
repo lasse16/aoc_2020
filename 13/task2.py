d4724c7c9f586cd5d5a709f2a23528bfaacf2b9d
Lasse Haffke <lasse@haffke.me>
2021-10-23 03:52:39 +0200

    Add massively improved solution for 13_02

--- 13/task2.py
+++ 13/task2.py
@@ -1,5 +1,30 @@
+import math
+
+
 def main():
     input = "input.txt"
+    bus_ids_and_offset_in_use = parse_input_into_bus_ids_and_offset_in_use(input)
+    bus_ids_and_offset_in_use = sorted(bus_ids_and_offset_in_use, key=lambda x: x[0])
+
+    bus_ids, _ = zip(*bus_ids_and_offset_in_use)
+
+    # All bus ids are primes
+    least_common_multiplier = math.prod(bus_ids)
+
+    timestep = 1
+    step_interval = 1
+
+    for bus_id, offset in bus_ids_and_offset_in_use:
+        # Everything after least_common_multiplier is the same modulo, so no solution should be possible to find
+        while timestep < least_common_multiplier:
+            if is_multiple_of(timestep + offset, bus_id):
+                step_interval *= bus_id
+                break
+            timestep += step_interval
+    print(f" solution {timestep}")
+
+
+def parse_input_into_bus_ids_and_offset_in_use(input):
     with open(input) as file:
         next(file)
         bus_ids = next(file).strip().split(",")
@@ -8,38 +33,8 @@ def main():
         for offset, bus_id in bus_ids_and_time_offset:
             if bus_id != "x":
                 bus_ids_and_offset_in_use.append((int(bus_id), offset))
-        print(bus_ids_and_offset_in_use)
-        max_bus_id_and_offset = max(bus_ids_and_offset_in_use, key=lambda x: x[0])
-        bus_ids_and_offset_in_use = shift_offsets(
-            bus_ids_and_offset_in_use, max_bus_id_and_offset[1]
-        )
-        print(bus_ids_and_offset_in_use)
-        solution_found = False
-        multiples = generate_multiples(max_bus_id_and_offset[0])
-        while not solution_found:
-            multiples_found = []
-            multiple = next(multiples)
-            for bus_id, offset in bus_ids_and_offset_in_use:
-                if not is_multiple_of(multiple + offset, bus_id):
-                    break
-                multiples_found.append(True)
-            solution_found = len(multiples_found) == len(bus_ids_and_offset_in_use)
-            if solution_found:
-                print(multiple)
 
-            if len(multiples_found) > 2:
-                print(f"{multiple} with {len(multiples_found)}")
-
-
-def shift_offsets(bus_ids_and_offsets, amount):
-    return [(bus_id, offset - amount) for bus_id, offset in bus_ids_and_offsets]
-
-
-def generate_multiples(multiple_base):
-    num = 1
-    while True:
-        yield multiple_base * num
-        num += 1
+        return bus_ids_and_offset_in_use
 
 
 def is_multiple_of(target, multiple_base):
