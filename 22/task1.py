960e0031310ac7a6fe4678bb6dcd14d9f2bbfc91
Lasse Haffke <lasse@haffke.me>
2021-10-27 00:20:46 +0200

    Add solution for 22_01

---
+++ 22/task1.py
@@ -0,0 +1,47 @@
+def main():
+    input = "input.txt"
+    deck_player1, deck_player2 = parse_input_into_decks(input)
+
+    while deck_player1 and deck_player2:
+        deck_player1, deck_player2 = play_one_round(deck_player1, deck_player2)
+    score = calculate_score(deck_player1, deck_player2)
+    print(score)
+
+
+def play_one_round(deck1, deck2):
+    player1_card = deck1.pop(0)
+    player2_card = deck2.pop(0)
+    if player1_card > player2_card:
+        deck1.append(player1_card)
+        deck1.append(player2_card)
+    else:
+        deck2.append(player2_card)
+        deck2.append(player1_card)
+
+    return deck1, deck2
+
+
+def calculate_score(deck1, deck2):
+    for deck in [deck for deck in [deck1, deck2] if deck]:
+        score = 0
+        for index, card in enumerate(deck[::-1], 1):
+            score += index * card
+        return score
+
+
+def parse_input_into_decks(input):
+    with open(input) as file:
+        deck_1, deck_2 = file.read().split("\n\n")
+
+        cards_1 = deck_1.splitlines(keepends=False)[1:]
+        cards_2 = deck_2.splitlines(keepends=False)[1:]
+
+        return parse_cards_to_deck(cards_1), parse_cards_to_deck(cards_2)
+
+
+def parse_cards_to_deck(cards):
+    return [int(card) for card in cards]
+
+
+if __name__ == "__main__":
+    main()
